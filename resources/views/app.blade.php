948e3bbf4ad41cb759396b9f355f52ce03126de4
philippejadin <philippe.jadin@gmail.com>
2017-03-24 15:48:26 +0100

    semantic ui conversion part 1 : navigation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,11 +2,12 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name=viewport content="width=device-width, initial-scale=1">
     <title>{{env('APP_NAME')}}</title>
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Semantic UI -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.css">
 
 
     <!-- Font awesome -->
@@ -17,7 +18,7 @@
 
 
     <!-- mobilizator specific css-->
-    {!! Html::style('/css/all.css?v7') !!}
+    <!--{!! Html::style('/css/all.css?v7') !!}-->
 
 
     <!-- head -->
@@ -28,34 +29,19 @@
 
     @include('partials.nav')
 
-    <div class="container-fluid nav-margin-top">
+    <div class="ui main container">
 
+        @include('partials.errors')
+        @yield('content')
 
-        @if (Auth::guest())
-            @include('partials.errors')
-            @yield('content')
-        @else
-
-
-            <div class="row">
-                <div class="col-lg-3">
-                    @include('partials.sidebar')
-                </div>
-
-                <div class="col-lg-9">
-                    @include('partials.errors')
-                    @yield('content')
-                </div>
-
-            </div>
-        @endif
-
-        <div class="credits">{{trans('messages.made_with')}} <a href="https://github.com/philippejadin/Mobilizator">Mobilizator</a></div>
+        <div class="footer">{{trans('messages.made_with')}} <a href="https://github.com/philippejadin/Mobilizator">Mobilizator</a></div>
     </div>
 
 
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.js"></script>
+
 
 
     <!-- js -->
